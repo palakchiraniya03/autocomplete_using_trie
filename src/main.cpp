b64dcd342346c1b0d4861b16ad563b726241be2b
Palak <palakchiraniya03>
2026-01-08 19:30:37 +0530

    tree classes added with some words

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 using namespace std;
+
+class TrieNode{
+  public:
+  TrieNode* children[26];
+  bool isEnd;
+
+  TrieNode(){
+    isEnd=false;
+    for(int i=0;i<26;i++){
+      children[i]=NULL;
+    }
+  }
+};
+
+class Trie{
+  public:
+  TrieNode* root;
+
+  Trie(){
+    root=new TrieNode();
+  }
+
+  void insert(string word){
+    TrieNode* curr=root;
+    for(char c:word){
+      int index=c-'a'
+
+      if(curr->children[index]==NULL){
+        curr->children[index]=new TrieNode();
+      }
+      curr=curr->children[index];
+    }
+    curr->isEnd=true;
+  }
+};
+
 int main(){
-    cout<<"Trie Autocomplete Project Started"<<endl;
-    return 0;
+  Trie trie;
+  trie.insert("apple");
+  trie.insert("app");
+  cout<<"inserted some words in the trie"<<endl;
+  return 0;
 }
